bb50296d1a045603a4f11ad7add3a3e406515771
kralinc <lelinxi@protonmail.com>
2022-08-03 18:04:06 -0500

    added eeeemail

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-window.addEventListener("DOMContentLoaded", addEventListeners);
+window.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    addEventListeners();
+    addEmail();
+}
 
 function addEventListeners() {
     document.querySelector(".navbar-toggler").addEventListener("click", toggleNavButton);
+}
+
+function addEmail() {
+    const sussy = [
+        99,
+        97,
+        99,
+        116,
+        117,
+        115,
+        100,
+        101,
+        118,
+        105,
+        116,
+        111,
+        64,
+        112,
+        114,
+        111,
+        116,
+        111,
+        110,
+        46,
+        109,
+        101
+      ];
+    let sus = "";
+    for (let c of sussy) {
+        sus += String.fromCharCode(c);
+    }
+    document.querySelector("#sus").textContent = sus;
 }
 
 function toggleNavButton() {
